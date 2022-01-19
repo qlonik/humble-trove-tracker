de6621641cfaa91ce6926545e0b57c8669c0fe99
Nikita Volodin <volodin.n@gmail.com>
2022-01-19 18:08:25 -0500

    feat: display collected availability of Humble Trove games

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,13 @@
 import { createTheme } from "@mui/material/styles";
 
-export default createTheme({});
+export default createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 1024,
+      xl: 1200,
+    },
+  },
+});

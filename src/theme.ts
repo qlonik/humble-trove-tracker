1bbe886b213cc61ffc427d0019857c162ee83c7c
Nikita Volodin <volodin.n@gmail.com>
2022-01-30 00:10:24 -0500

    feat: set dark mode and use humble background color

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,12 @@
 import { createTheme } from "@mui/material/styles";
 
 export default createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#3e4451",
+    },
+  },
   breakpoints: {
     values: {
       xs: 0,

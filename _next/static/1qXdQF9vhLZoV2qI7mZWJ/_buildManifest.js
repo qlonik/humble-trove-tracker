cc8e78a2665fd02034eb446beabc7262f755e8b8
qlonik <volodin.n@gmail.com>
2022-02-15 05:57:17 +0000

    Deploying to gh-pages from @ qlonik/humble-trove-tracker@740b37bb02865395e57ed4a030300be07f6f476b 🚀

--- _next/static/1qXdQF9vhLZoV2qI7mZWJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/706-bcfb537337ed70ed.js","static/chunks/pages/index-08c38972dcc80292.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
